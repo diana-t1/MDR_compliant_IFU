e1e00d25d6c00ee8727952748d373e623f22d584
diana-t1 <186348766+diana-t1@users.noreply.github.com>
2025-05-23 07:42:16 +0200

    Initial commit

---
+++ Data/Search.js
@@ -0,0 +1,1 @@
+define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/A_MDR_basics/Is_this_guide_for_You.htm'],s:['1'],p:[['1',14],['iso',1],['where',9]]});
---
+++ Resources/Scripts/require.config.js
@@ -0,0 +1,3 @@
+require.config({
+    urlArgs: 't=638835690209934754'
+});
